bb64c2435e8fc72de996c7c5d74bccbc0343ca75
maruf-hasann <afnafmafeee@gmail.com>
2026-04-18 12:26:08 +0600

    fix

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://dark-eight-omega.vercel.app/",
 
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.token; // from redux state
